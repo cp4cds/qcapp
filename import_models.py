a2fc12f44d04e6eba1264a4386c04bf0956a7844
Ruth Petrie <root@cp4cds-app1-test.ceda.ac.uk>
2017-03-22 15:47:07 +0000

    Update volume calculation

--- import_models.py
+++ import_models.py
@@ -2,6 +2,15 @@ import django
 django.setup()
 
 from qcapp.models import *
+from django.db.models import Count, Max, Min, Sum, Avg
+import collections, os, timeit, datetime, time
+import requests, itertools
+# IMPORT QC STUFF (only works in venv27)
+from ceda_cc import c4
+from cfchecker.cfchecks import CFVersion, CFChecker, STANDARDNAME, AREATYPES, newest_version
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings()
+
 print "Counts..."
 print "Dataset:", Dataset.objects.count()
 print "DataFile:", DataFile.objects.count()
