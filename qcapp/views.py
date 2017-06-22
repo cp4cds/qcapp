72bc51afbb039c7a50876525296ef33c4549c126
Ruth Petrie <root@cp4cds-app1-test.ceda.ac.uk>
2017-06-22 15:37:37 +0100

    Update timeseries and single file view to go through variable_qc() and filter ERROR 4#

--- qcapp/views.py
+++ qcapp/views.py
@@ -76,73 +76,34 @@ def get_total_qc_errors(qcfile):
     errors = {}
     errors['global'] = qc_errors.filter(error_type='global').count()
     errors['variable'] = qc_errors.filter(error_type='variable').count()
-    errors['other'] = qc_errors.filter(error_type='other').count()
+    errors['other'] = qc_errors.filter(error_type='other').exclude(error_msg__contains="ERROR (4)").count()
 
     return errors
 
-def variable_file_qc(request, ncfile):
 
-#    files = DataFile.objects.filter(ncfile=ncfile, dataset__version=version)
-    files = DataFile.objects.filter(ncfile=ncfile)
-    file = files.first()
-    qc_errors = file.qcerror_set.all()
-    qc_cf_errors = file.qcerror_set.filter(check_type='CF')
-    qc_cedacc_errors = file.qcerror_set.filter(check_type='CEDA-CC')
-
-    global_cf_errs = qc_cf_errors.filter(error_type='global')
-    var_cf_errs = qc_cf_errors.filter(error_type='variable')
-    other_cf_errs = qc_cf_errors.filter(error_type='other')
-
-    global_cedacc_errs = qc_cedacc_errors.filter(error_type='global')
-    var_cedacc_errs = qc_cedacc_errors.filter(error_type='variable')
-    other_cedacc_errs = qc_cedacc_errors.filter(error_type='other')
-
-    global_total_errs = qc_errors.filter(error_type='global')
-    var_total_errs = qc_errors.filter(error_type='variable')
-    other_total_errs = qc_errors.filter(error_type='other')
-
-    qc_error_counts = {'cf_global': global_cf_errs.count(), 'cf_variable': var_cf_errs.count(), 'cf_other': other_cf_errs.count(),
-                       'cedacc_global': global_cedacc_errs.count(), 'cedacc_variable': var_cedacc_errs.count(), 'cedacc_other': other_cedacc_errs.count(),
-                       'global': global_total_errs.count(), 'variable': var_total_errs.count(), 'other': other_total_errs.count()}
-
-    #    filepath = os.path.join( "/group_workspaces/jasmin/cp4cds1/qc/QCchecks/CEDACC-OUTPUT/LASG-CESS/FGOALS-g2/historical/Amon/v1/",
-    #                             qc_errors.first().report_filepath)
-
-    # ds_id = os.path.dirname(file.filepath).replace('/', '.')[1:]
-    # filen = os.path.basename(file.filepath)
-    # title = "Variable %s dataset: \n %s \n %s" % (variable, ds_id, os.path.basename(filename))
-    title = "Variable quality control summary"
-    dataset_id = file.dataset.dataset_id
-    filename = os.path.basename(file.archive_path)
-
-    return render(request, 'qcapp/variable-file-qc.html', {'page_title': title, 'dataset_id': dataset_id, 'filename': filename,
-                                                  'qc_cf_errors': qc_cf_errors, 'qc_cedacc_errors': qc_cedacc_errors,
-                                                  'qc_error_counts': qc_error_counts})
+def variable_qc(request, ncfile):
 
 
-def variable_timeseries_qc(request):
+    if DataFile.objects.filter(ncfile=ncfile).first().timeseries == True:
 
-    title = "Variable timeseries QC information"
+        title = "Variable timeseries QC information"
+        var, table, model, expt, ens = ncfile.split('_')[:-1]
+        timeseries_df_errors = {}
 
-    ncfile = 'tos_Omon_GFDL-ESM2M_rcp45_r1i1p1_201601-202012.nc'
-#    ncfile = 'tsice_OImon_inmcm4_historical_r1i1p1_185001-200512.nc'
-#    ncfile = 'zos_Omon_ACCESS1-3_rcp45_r1i1p1_200601-210012.nc'
-    var, table, model, expt, ens = ncfile.split('_')[:-1]
-    timeseries_df_errors = {}
+        for datafile in DataFile.objects.filter(dataset__variable=var, dataset__cmor_table=table, dataset__model=model,
+                                                dataset__experiment=expt, dataset__ensemble=ens, timeseries=True):
+            timeseries_df_errors[datafile.ncfile] = get_total_qc_errors(datafile.ncfile)
+            total_errors = sum_timeseries_qc_errors(timeseries_df_errors)
 
-    for datafile in DataFile.objects.filter(dataset__variable=var, dataset__cmor_table=table, dataset__model=model,
-                                            dataset__experiment=expt, dataset__ensemble=ens, timeseries=True):
-        timeseries_df_errors[datafile.ncfile] = get_total_qc_errors(datafile.ncfile)
-        total_errors = sum_timeseries_qc_errors(timeseries_df_errors)
-
-    if len(timeseries_df_errors) > 0:
         return render(request, 'qcapp/variable-timeseries-qc.html',
-                      {'page_title': title, 'timeseries_df_errors': timeseries_df_errors, 'total_errors': total_errors})
+                      {'page_title': title, 'timeseries_df_errors': timeseries_df_errors,
+                       'total_errors': total_errors})
+
     else:
-        files = DataFile.objects.filter(ncfile=ncfile)
-        file = files.first()
-        qc_errors = file.qcerror_set.all()
-        qc_cf_errors = file.qcerror_set.filter(check_type='CF')
+        print "not timeseries dataset"
+        file = DataFile.objects.filter(ncfile=ncfile).first()
+        qc_errors = file.qcerror_set.all().exclude(error_msg__contains="ERROR (4)")
+        qc_cf_errors = file.qcerror_set.filter(check_type='CF').exclude(error_msg__contains="ERROR (4)")
         qc_cedacc_errors = file.qcerror_set.filter(check_type='CEDA-CC')
 
         global_cf_errs = qc_cf_errors.filter(error_type='global')
@@ -164,12 +125,64 @@ def variable_timeseries_qc(request):
                            'global': global_total_errs.count(), 'variable': var_total_errs.count(),
                            'other': other_total_errs.count()}
 
-        #    filepath = os.path.join( "/group_workspaces/jasmin/cp4cds1/qc/QCchecks/CEDACC-OUTPUT/LASG-CESS/FGOALS-g2/historical/Amon/v1/",
-        #                             qc_errors.first().report_filepath)
+        title = "Variable quality control summary"
+        dataset_id = file.dataset.dataset_id
+        filename = os.path.basename(file.archive_path)
 
-        # ds_id = os.path.dirname(file.filepath).replace('/', '.')[1:]
-        # filen = os.path.basename(file.filepath)
-        # title = "Variable %s dataset: \n %s \n %s" % (variable, ds_id, os.path.basename(filename))
+        return render(request, 'qcapp/variable-file-qc.html',
+                      {'page_title': title, 'dataset_id': dataset_id, 'filename': filename,
+                       'qc_cf_errors': qc_cf_errors, 'qc_cedacc_errors': qc_cedacc_errors,
+                       'qc_error_counts': qc_error_counts})
+
+
+def variable_file_qc(request, ncfile):
+
+    files = DataFile.objects.filter(ncfile=ncfile)
+    file = files.first()
+    qc_errors = file.qcerror_set.all().exclude(error_msg__contains="ERROR (4)")
+    qc_cf_errors = file.qcerror_set.filter(check_type='CF').exclude(error_msg__contains="ERROR (4)")
+    qc_cedacc_errors = file.qcerror_set.filter(check_type='CEDA-CC')
+
+    global_cf_errs = qc_cf_errors.filter(error_type='global')
+    var_cf_errs = qc_cf_errors.filter(error_type='variable')
+    other_cf_errs = qc_cf_errors.filter(error_type='other')
+
+    global_cedacc_errs = qc_cedacc_errors.filter(error_type='global')
+    var_cedacc_errs = qc_cedacc_errors.filter(error_type='variable')
+    other_cedacc_errs = qc_cedacc_errors.filter(error_type='other')
+
+    global_total_errs = qc_errors.filter(error_type='global')
+    var_total_errs = qc_errors.filter(error_type='variable')
+    other_total_errs = qc_errors.filter(error_type='other')
+
+    qc_error_counts = {'cf_global': global_cf_errs.count(), 'cf_variable': var_cf_errs.count(), 'cf_other': other_cf_errs.count(),
+                       'cedacc_global': global_cedacc_errs.count(), 'cedacc_variable': var_cedacc_errs.count(), 'cedacc_other': other_cedacc_errs.count(),
+                       'global': global_total_errs.count(), 'variable': var_total_errs.count(), 'other': other_total_errs.count()}
+
+    title = "Variable quality control summary"
+    dataset_id = file.dataset.dataset_id
+    filename = os.path.basename(file.archive_path)
+
+    return render(request, 'qcapp/variable-file-qc.html', {'page_title': title, 'dataset_id': dataset_id, 'filename': filename,
+                                                  'qc_cf_errors': qc_cf_errors, 'qc_cedacc_errors': qc_cedacc_errors,
+                                                  'qc_error_counts': qc_error_counts})
+
+
+def variable_timeseries_qc(request, ncfile):
+
+    title = "Variable timeseries QC information"
+
+    var, table, model, expt, ens = ncfile.split('_')[:-1]
+    timeseries_df_errors = {}
+
+    for datafile in DataFile.objects.filter(dataset__variable=var, dataset__cmor_table=table, dataset__model=model,
+                                            dataset__experiment=expt, dataset__ensemble=ens, timeseries=True):
+        timeseries_df_errors[datafile.ncfile] = get_total_qc_errors(datafile.ncfile)
+        total_errors = sum_timeseries_qc_errors(timeseries_df_errors)
+
+        return render(request, 'qcapp/variable-timeseries-qc.html',
+                      {'page_title': title, 'timeseries_df_errors': timeseries_df_errors, 'total_errors': total_errors})
+
         title = "Variable quality control summary"
         dataset_id = file.dataset.dataset_id
         filename = os.path.basename(file.archive_path)
--- qcproj/urls.py
+++ qcproj/urls.py
@@ -21,8 +21,9 @@ import qcapp.views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^documentation/', qcapp.views.documentation),
+    url(r'^variable-qc/(?P<ncfile>\S+)', qcapp.views.variable_qc, name="variable-qc"),
     url(r'^variable-file-qc/(?P<ncfile>\S+)', qcapp.views.variable_file_qc, name="variable-file-qc"),
-    url(r'^variable-timeseries-qc/', qcapp.views.variable_timeseries_qc, name="variable-timeseries-qc"),
+    url(r'^variable-timeseries-qc/(?P<ncfile>\S+)', qcapp.views.variable_timeseries_qc, name="variable-timeseries-qc"),
     url(r'^variable-dataset-qc/(?P<variable>\S+)', qcapp.views.variable_dataset_qc, name="variable-dataset-qc"),
     url(r'^variable-summary-qc/', qcapp.views.variable_summary_qc, name="variable-summary-qc"),
     url(r'^data-spec/', qcapp.views.data_spec, name="data-spec"),
