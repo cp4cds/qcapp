734afb4a22ed7dd382632bdb8b13842d6f524a45
Ruth Petrie <rpetrie@jasmin-sci2.ceda.ac.uk>
2017-10-25 11:32:25 +0100

    Setup for lotus

--- submit-lotus.sh
+++ submit-lotus.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-odir=/group_workspaces/jasmin2/cp4cds1/qc/qc-app/qcapp/lotus-logs
+odir=/group_workspaces/jasmin2/cp4cds1/qc/qc-app2/qcapp/lotus-logs
 mkdir -p $odir
 
 var=$1
@@ -8,5 +8,5 @@ table=$2
 freq=$3
 expt=$4
 
-bsub -J -o $odir/%J.out -W 24:00 ./run_qc_lotus.sh $var $table $freq $expt
+bsub -o $odir/%J.out -W 24:00 ./run_qc_lotus.sh $var $table $freq $expt
 
