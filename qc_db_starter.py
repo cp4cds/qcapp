d4e1c6620b9b6968c767dac6d74cc6b1cbd1de6e
Ruth Petrie <rpetrie@jasmin-sci1.ceda.ac.uk>
2017-12-07 16:19:54 +0000

    Modify to pass all args from qc_db_starter.py

--- qc_db_starter.py
+++ qc_db_starter.py
@@ -21,6 +21,7 @@ from subprocess import call
 ####
 # Specify lotus output dir here
 ####
+command_line_args = "--check_up_to_date"
 lotus_out = 'run-time-checker'
 files = ["ancil_files/cp4cds_data_requirements.txt",
          "ancil_files/magic_additional_data.txt"]
@@ -48,7 +49,7 @@ for file in files:
                 frequency = line.split(',')[2].strip()
                 if DEBUG: print variable, table, frequency
 
-                lotus_cmd = ['./submit-lotus.sh', variable, table, frequency, lotus_out]
+                lotus_cmd = ['./submit-lotus.sh', variable, table, frequency, lotus_out, command_line_args]
                 res = call(lotus_cmd)
                 call(['sleep', '5'])
             lineno += 1
