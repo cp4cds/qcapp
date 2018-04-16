e0c216787e010bfb82b30a7163055caf3927528b
Ruth Petrie <rpetrie@jasmin-sci1.ceda.ac.uk>
2018-04-16 08:34:15 +0100

    Tidy up

--- submit_db_builder_lotus.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Run from /group_workspaces/jasmin2/cp4cds1/qc/qc-app2/qcapp
-odir=lotus-logs/db-builder/
-mkdir -p $odir
-
-for batch in $(seq 0 248); do
-    bsub -o ./$odir/%J.out -W 24:00 ./setup_environment_lotus.sh $batch
-done
