c0bd037265a39aac7c9209891a82e2ed99172feb
rsmith013 <richard.d.smith@stfc.ac.uk>
2019-10-03 16:50:09 +0100

    updated url patterns to new way

--- qcproj/urls.py
+++ qcproj/urls.py
@@ -13,23 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.urls import path
 from django.contrib import admin
 
 from qcapp.views import *
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', HomeView.as_view(), name="home"),
-    url(r'^model-details/', model_details, name="model-details"),
-    url(r'^variable-details/', variable_details, name="variable-details"),
-    url(r'^data-availability/', data_availability_matrix, name="data-availability-matrix"),
-    url(r'^help/', HelpView.as_view(), name="help"),
-
+    path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name="home"),
+    path('model-details/', model_details, name="model-details"),
+    path('variable-details/', variable_details, name="variable-details"),
+    path('data-availability/', data_availability_matrix, name="data-availability-matrix"),
+    path('help/', HelpView.as_view(), name="help"),
 
     # Ajax endpoints
-    url(r'^facet-filter/(?P<model>\S+)/(?P<experiment>\S+)',
-        facet_filter, name="facet-filter"),
-    url(r'^get_variable_details/(?P<variable>\S+)/(?P<table>\S+)/(?P<freq>\S+)', get_variable_details, name="get_varaible_details"),
+    path('facet-filter/<str:model>/<str:experiment>', facet_filter, name="facet-filter"),
+    path('get_variable_details/<str:variable>/<str:table>/<str:freq>', get_variable_details,
+         name="get_varaible_details"),
 
 ]
